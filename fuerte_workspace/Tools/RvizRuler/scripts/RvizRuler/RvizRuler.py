18c50de444c876502b9d3ef66b63e570b332a569
AtsushiSakai <asakai.amsl@gmail.com>
2014-04-30 08:28:36 +0900

    all functions are added

--- fuerte_workspace/Tools/RvizRuler/scripts/RvizRuler/RvizRuler.py
+++ fuerte_workspace/Tools/RvizRuler/scripts/RvizRuler/RvizRuler.py
@@ -21,6 +21,9 @@ import tf
 from tf.transformations import euler_from_quaternion
 import math
 
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
+
 count=0
 
 class MyPlugin(Plugin):
@@ -29,8 +32,16 @@ class MyPlugin(Plugin):
         # Give QObjects reasonable names
         self.setObjectName('MyPlugin')
 
-        #final Inputの取得 TODO:ちゃんと表示させるようにする
+        #Rvizから位置を受け取るsubscriver
         rospy.Subscriber('initialpose',PoseWithCovarianceStamped,self.InitialPoseCallback)
+        global pub
+        pub = rospy.Publisher('line', Marker)
+        #グローバル変数の初期化
+        global InitMsg
+        InitMsg=0
+        global preInitMsg
+        preInitMsg=0
+
 
         # Process standalone plugin command-line arguments
         from argparse import ArgumentParser
@@ -87,15 +98,78 @@ class MyPlugin(Plugin):
           # Usually used to open a configuration dialog
          
   def InitialPoseCallback(self,data):
+    #rvizからのデータを取得するCallback関数
     global InitMsg
-    InitMsg=data
+    global preInitMsg
+    if InitMsg !=0:
+      preInitMsg=InitMsg  #global変数に一つ前のデータを格納
+    InitMsg=data        #global変数に現在のデータを格納
 
   def printdata(self):
+    #GUIにデータを表示する関数
     global InitMsg
-    x="%3.2f" %(InitMsg.pose.pose.position.x)
+    if isinstance(InitMsg, int):#InitMsgが初期化されていない
+      return 0
+    fx=InitMsg.pose.pose.position.x
+    fy=InitMsg.pose.pose.position.y
+    x="%3.2f" %(fx)
     self._widget.x.setPlainText(x)
-    y="%3.2f" %(InitMsg.pose.pose.position.y)
+    y="%3.2f" %(fy)
     self._widget.y.setPlainText(y) 
-    (roll,pitch,yaw) = euler_from_quaternion([InitMsg.pose.pose.orientation.x,InitMsg.pose.pose.orientation.y,InitMsg.pose.pose.orientation.z,InitMsg.pose.pose.orientation.w]);
+
+    #オイラー角の取得
+    ox=InitMsg.pose.pose.orientation.x
+    oy=InitMsg.pose.pose.orientation.y
+    oz=InitMsg.pose.pose.orientation.z
+    ow=InitMsg.pose.pose.orientation.w
+    (roll,pitch,yaw) = euler_from_quaternion([ox,oy,oz,ow]);
     yaw="%3.2f" %(yaw*180/math.pi)
     self._widget.yaw.setPlainText(yaw)
+
+    #原点からの距離を計算
+    dorigin="%3.2f" %(math.sqrt(fx*fx+fy*fy))
+    self._widget.dorigin.setPlainText(dorigin)
+
+    #原点からの角度を計算
+    thetaorigin="%3.2f" %(math.atan2(fy,fx)*180/math.pi)
+    self._widget.thetaorigin.setPlainText(thetaorigin)
+
+    #前回の点からの距離を計算
+    if isinstance(preInitMsg, int): #preInitMsgが初期化されていない
+      return
+    dx=fx-preInitMsg.pose.pose.position.x #xの変化量[m]
+    dy=fy-preInitMsg.pose.pose.position.y #yの変化量[m]
+    dprepos="%3.2f" %(math.sqrt(dx*dx+dy*dy)) #ノルム距離の計算
+    self._widget.dprepos.setPlainText(dprepos)
+
+    #前回の点からの角度を計算
+    thetaprepos="%3.2f" %(math.atan2(dy,dx)*180/math.pi)  #角度[rad]の計算
+    self._widget.thetaprepos.setPlainText(thetaprepos)
+
+    #線描画用Markerのpublish
+    marker = Marker()
+    marker.header.frame_id = "nav";
+    marker.ns = "Line";
+    marker.type = marker.LINE_STRIP;
+    marker.action = marker.ADD;
+    marker.frame_locked = True;
+    marker.scale.x = 0.5; #線の太さ
+    marker.color.a = 1.0; #透明度
+    marker.color.g = 1.0; #色は緑
+    marker.lifetime.secs = 1.0;#表示時間[s]
+
+    p=Point();#始点
+    p.x=InitMsg.pose.pose.position.x
+    p.y=InitMsg.pose.pose.position.y
+    marker.points.append(p);
+
+    p=Point();#終点
+    p.x=preInitMsg.pose.pose.position.x
+    p.y=preInitMsg.pose.pose.position.y
+    marker.points.append(p);
+
+    pub.publish(marker);#publish
+
+
+
+
